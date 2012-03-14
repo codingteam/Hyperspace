4ed08b40984f241678626780135a7a0c19189dd6
ForNeVeR <neverthness@gmail.com>
2012-03-14 23:42:48 +0700

    gravity: fixed force and acceleration calculations. Temporary increased gravity constant.

--- src/hyperspace/gravity.clj
+++ src/hyperspace/gravity.clj
@@ -1,20 +1,27 @@
 (ns hyperspace.gravity
-  (:use (hyperspace geometry)))
+  (:use (hyperspace geometry))
+  (:import (hyperspace.geometry vector2)))
 
-(def gravity-constant 6.6725e-11)
+(def gravity-constant 3);6.6725e-11) TODO: Fix this!
 (def bullet-mass 1)
 
 (defn gravity-force
   [m1 m2 distance]
   (/ (* gravity-constant m1 m2) (Math/pow distance 2)))
 
+(defn planet-gravity-force
+  [bullet planet]
+  (let [planet-center (:center planet)
+        bullet-center (:center bullet)
+        planet-mass (:mass planet)
+        distance (point-distance planet-center bullet-center)
+        force (gravity-force planet-mass bullet-mass distance)
+        acceleration (/ force bullet-mass)
+        angle (bearing-to bullet-center planet-center)]
+    (make-vector-radial acceleration angle)))
+
 (defn get-acceleration
   [bullet planets]
-  (let [planet-center (:center planets)
-        bullet-center (:center bullet)
-        force (reduce #(+ %1 (gravity-force
-                               bullet-mass (:mass %2)
-                               (point-distance planet-center bullet-center)))
-                      0
-                      planets)]
-    (/ force bullet-mass)))
+  (reduce #(vector-sum %1 (planet-gravity-force bullet %2))
+          (vector2. 0 0)
+          planets))
