d80af730e2a1cc44c8c0f49145439e58f0f08ee8
ForNeVeR <neverthness@gmail.com>
2012-05-03 20:15:58 +0700

    Fixed game tests. Added bullet-move-test.

--- test/hyperspace/test/game.clj
+++ test/hyperspace/test/game.clj
@@ -1,8 +1,10 @@
 (ns hyperspace.test.game
   (:use (clojure test)
+        (hyperspace.test utils)
         (hyperspace game
                     geometry
-                    world))
+                    gravity
+                    world)))
 
 (deftest player-params-test
   (let [player1 (make-player (make-point 0 0) 0 0 "p1")
@@ -15,3 +17,18 @@
            (set players)))
     (is (= (set (:players world2))
            (set [player1 player2-changed])))))
+
+(deftest bullet-move-test
+  (let [planet1 (make-planet (make-point 0 0) 1 100)
+        planet2 (make-planet (make-point 0 5) 1 200)
+        planets [planet1 planet2]
+        bullet (make-bullet (make-point 5 5) (make-vector 1 1))
+        acceleration (get-acceleration bullet planets)
+        new-velocity (vector-sum (:velocity bullet) acceleration)
+        new-bullet (move-bullet bullet planets)]
+    (is (almost= (:x (:center new-bullet)) 6))
+    (is (almost= (:y (:center new-bullet)) 6))
+    (is (almost= (:x (:velocity new-bullet))
+                 (:x new-velocity)))
+    (is (almost= (:y (:velocity new-bullet))
+                 (:y new-velocity)))))
