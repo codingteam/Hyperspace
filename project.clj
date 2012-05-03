a66b9d2fd915a6d8ec88dd4765e04bb74927a77d
ForNeVeR <neverthness@gmail.com>
2012-05-03 22:39:09 +0700

    Moved to clojure 1.4.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hyperspace "1.0.0-SNAPSHOT"
   :description "Simple 2D game written in Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.lwjgl/lwjgl "2.7.1"]
                  [org.lwjgl/lwjgl-util "2.7.1"]
                  [org.lwjgl/lwjgl-native-platform "2.7.1"]]
