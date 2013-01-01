32b516a1672599ffaf048e7cb6041a8095e40ef0
ForNeVeR <neverthness@gmail.com>
2013-01-01 23:49:18 +0700

    simulation: started adding tests for update-particle function.

--- test/hyperspace/test/library/sumulation.clj
+++ test/hyperspace/test/library/sumulation.clj
@@ -4,6 +4,7 @@
         [midje.sweet])
   (:require [hyperspace.library.world :as world]))
 
+(def missile-fixture (world/make-missile [0 0] [10 0]))
 (def player-fixture (world/make-player [10 10]))
 (def nonexistent-player-fixture (world/make-player [20 20]))
 (def world-fixture (-> (world/create 800 600)
@@ -13,5 +14,11 @@
   (kill-player world-fixture player-fixture) => (assoc world-fixture :players [(assoc player-fixture :status :dead)])
   (kill-player world-fixture nonexistent-player-fixture) => world-fixture)
 
-;;; TODO: Test the update-particle function.
+(facts "about update-particle function"
+  (:position (update-particle missile-fixture [] 1)) => (just [(almost= 1e-2)
+                                                               (almost= 0)])
+  ;; TODO: test with planet
+  ;; TODO: test with two planets
+  )
+
 ;;; TODO: Test the fire function.
