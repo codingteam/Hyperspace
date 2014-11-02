eea00cf0a7e2cc36a116c97d6eb25c7b42c7c907
Friedrich von Never <friedrich@fornever.me>
2014-11-02 22:20:17 +0600

    Remove the Flyway and Thrift stuff.

--- resources/db/migration/V1__Add_user_table.sql
+++
@@ -1,9 +0,0 @@
-create table User (
-  id integer primary key,
-  login varchar(255),
-  passwordHash varchar(32)
-);
-
-alter table User add constraint login_unique unique(login);
-
-create unique index login_index on User(login);
