bcddc801dd784c57bf7bca991951b8da4c2f1e5d
Friedrich von Never <friedrich@fornever.me>
2014-11-04 17:07:44 +0600

    Fix artifact version.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Simple 2D game written in Clojure."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.lwjgl.lwjgl/lwjgl "2.9.1"]
-                 [org.ru.codingteam/lwjgl-platform-natives "2.9.1"]
+                 [ru.org.codingteam/lwjgl-platform-natives "2.9.1"]
                  [org.clojars.rexim/jinput-platform-natives "2.0.5"]]
   :dev-dependencies [[midje "1.4.0"]
                      [lein-midje "1.0.10"]]
