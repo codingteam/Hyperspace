fd92645f829dc8ab613a26686e14135a2c0fae5e
Friedrich von Never <friedrich@fornever.me>
2014-11-10 23:39:56 +0600

    Add table for games definitions.

--- src/hyperspace/server/database/migrations.clj
+++ src/hyperspace/server/database/migrations.clj
@@ -11,4 +11,18 @@
                                          [:session [:varchar 16] :null true :unique true]])
                        (ch/create-index :users [:login])]])
 
-(defchangelog changelog "hyperspace-server" [add-users-table])
+(def add-games-table ["id=2"
+                      "author=ForNeVeR"
+                      [(ch/create-table :games
+                                        [[:id :int :null false :pk true :autoinc true]
+                                         [:players :int :null false]
+                                         [:state [:varchar 16]]])
+                       (ch/create-index :games [:state])
+                       (ch/create-table :game-users
+                                        [[:user-id :int
+                                          :references "users(id)" :foreign-key-name :game-users-users]
+                                         [:game-id :int
+                                          :references "games(id)" :foreign-key-name :game-users-games]])]])
+
+(defchangelog changelog "hyperspace-server" [add-users-table
+                                             add-games-table])
