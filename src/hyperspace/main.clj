a39bb4baa7f43d3a607428752fb2e6ac0380ac67
Friedrich von Never <friedrich@fornever.me>
2016-05-08 21:28:04 +0700

    Main: fix server starting

--- src/hyperspace/main.clj
+++ src/hyperspace/main.clj
@@ -1,8 +1,8 @@
 (ns hyperspace.main
   (:require [hyperspace.client.main :as client]
-            [hyperspace.server.main :as server]))
+            [hyperspace.server.server :as server]))
 
 (defn -main [application & args]
   (cond
     (= application "client") (apply client/run args)
-    (= application "server") (apply server/run args)))
+    (= application "server") (apply server/start args)))
