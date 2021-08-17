8cf011feffc80d1f7c0a49270c8f64b80ae2e0fb
Friedrich von Never <friedrich@fornever.me>
2021-08-17 23:50:56 +0700

    world: convert tests to clojure.test

--- test/hyperspace/test/library/world.clj
+++ test/hyperspace/test/library/world.clj
@@ -1,10 +1,10 @@
 (ns hyperspace.test.library.world
-  (:use [hyperspace.library.world]
-        [hyperspace.test.checkers]
-        [midje.sweet]))
+  (:use [clojure.test]
+        [hyperspace.library.world]
+        [hyperspace.test.checkers]))
 
-(facts "about make-missile function"
-  (make-missile [10 20] [30 40]) => {:position [10 20]
-                                     :velocity [30 40]
-                                     :radius   missile-radius
-                                     :mass     missile-mass})
+(deftest make-missile-tests
+  (is (= (make-missile [10 20] [30 40]) {:position [10 20]
+                                         :velocity [30 40]
+                                         :radius   missile-radius
+                                         :mass     missile-mass})))
