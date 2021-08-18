b7a686adc117bbdf1898c79d4fa3ad44443b3def
Friedrich von Never <friedrich@fornever.me>
2021-08-19 02:06:27 +0700

    (#43) Build: copy dependencies from project.clj to Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,24 @@ repositories {
 }
 
 dependencies {
-    implementation("org.clojure:clojure:1.10.0")
+    implementation("azql:azql:0.2.0")
+    implementation("clj-liquibase:clj-liquibase:0.5.2")
+    implementation("com.h2database:h2:1.3.173")
+    implementation("crypto-password:crypto-password:0.1.3")
+    implementation("crypto-random:crypto-random:1.2.0")
+    implementation("log4j:log4j:1.2.17")
+    implementation("metosin:compojure-api:0.16.0")
+    implementation("metosin:ring-http-response:0.5.0")
+    implementation("metosin:ring-swagger-ui:2.0.17")
+    implementation("metosin:ring-swagger:0.13.0")
+    implementation("org.clojure:clojure-contrib:1.2.0")
+    implementation("org.clojure:clojure:1.10.3")
+    implementation("org.clojure:data.json:0.2.2")
+    implementation("org.clojure:tools.logging:0.2.3")
+    implementation("org.clojure:tools.logging:0.2.3")
+    implementation("org.lwjgl.lwjgl:lwjgl:2.9.1")
+    implementation("ru.org.codingteam:jinput-platform-natives:2.0.6")
+    implementation("ru.org.codingteam:lwjgl-platform-natives:2.9.1")
     testRuntimeOnly("org.ajoberstar:jovial:0.3.0")
 }
 
